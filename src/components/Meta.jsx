8e5108235bf2e5e16f8246388ea7d26e5bf092d5
somtodev <leroynnalue65@gmail.com>
2023-10-10 18:45:28 +0100

    Update: Accent Color For Web Browsers

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -1,49 +1,43 @@
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 export default function Meta({ title, page }) {
-    return (
-        <Helmet>
-            <title>{title}</title>
-            <meta name="description" content="Your page description." />
-            <meta name="robots" content="index, follow" />
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content="Your page description." />
+      <meta name="robots" content="index, follow" />
 
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-            />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-            {/* Open Graph tags */}
-            <meta property="og:title" content="Somtochukwu" />
-            <meta property="og:description" content="SWD | Personal Website" />
-            <meta property="og:image" content="/profile.png" />
-            <meta property="og:url" content="https://somtodev.netlify.app" />
+      {/* Open Graph tags */}
+      <meta property="og:title" content="Somtochukwu" />
+      <meta property="og:description" content="SWD | Personal Website" />
+      <meta property="og:image" content="/profile.png" />
+      <meta property="og:url" content="https://somtodev.netlify.app" />
 
-            {/* Twitter Card tags */}
-            <meta name="twitter:title" content="Somtochukwu" />
-            <meta name="twitter:description" content="Software Developer" />
-            <meta name="twitter:image" content="/profile.png" />
-        </Helmet>
-    );
+      {/* Twitter Card tags */}
+      <meta name="twitter:title" content="Somtochukwu" />
+      <meta name="twitter:description" content="Software Developer" />
+      <meta name="twitter:image" content="/profile.png" />
+
+      {/* Accent Color  */}
+      <meta name="theme-color" content="#111" />
+    </Helmet>
+  );
 }
 
 function BlogMeta() {
-    return (
-        <>
-            {/* Open Graph tags */}
-            <meta property="og:title" content="Somtochukwu | Blog" />
-            <meta property="og:description" content="Somto Dev's Blog" />
-            <meta property="og:image" content="/profile.png" />
-            <meta
-                property="og:url"
-                content="https://somtodev.netlify.app/blog"
-            />
-            {/* Twitter Card tags */}
-            <meta name="twitter:title" content="Somtochukwu | Blog" />
-            <meta name="twitter:description" content="Somto Dev's Blog" />
-            <meta
-                name="twitter:image"
-                content="https://somtodev.netlify.app/blog"
-            />
-        </>
-    );
+  return (
+    <>
+      {/* Open Graph tags */}
+      <meta property="og:title" content="Somtochukwu | Blog" />
+      <meta property="og:description" content="Somto Dev's Blog" />
+      <meta property="og:image" content="/profile.png" />
+      <meta property="og:url" content="https://somtodev.netlify.app/blog" />
+      {/* Twitter Card tags */}
+      <meta name="twitter:title" content="Somtochukwu | Blog" />
+      <meta name="twitter:description" content="Somto Dev's Blog" />
+      <meta name="twitter:image" content="https://somtodev.netlify.app/blog" />
+    </>
+  );
 }
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,23 +1,34 @@
 export default function Hero() {
-    return (
-        <section className="py-4 my-6">
-            <div>
-                <h1 className="text-3xl text-primary font-header font-bold md:text-6xl lg:text-8xl">
-                    Nice to meet you!<br/>
-                    I'm<br/><span className="text-secondary">Somtochukwu</span>.
-                </h1>
-                <em className="opacity-80 text-secondary">_somtodev</em>
-            </div>
-            <p className="text-lg font-light my-4 opacity-75 ">
-                <span className="font-normal">A software engineering student at
-                Aptech.</span><br/> Ever since I graduated high school, hitting my keyboard for a solution is something I'm
-                passionate about, though sometimes I always try to outthink
-                myself on a better solution before hitting my keyboard. I'm an
-                aspiring backend developer who has dabbled in the ends of
-                frontend web development for over a year, good enough to call
-                myself one.
-            </p>
-            <a href="mailto:leroynnalue65@gmail.com" className="px-3 py-2 text-md bg-border bg-opacity-75 rounded-md transition-all hover:bg-opacity-100">Mail Me</a>
-        </section>
-    );
+  return (
+    <section className="py-4 my-6">
+      <div>
+        <h1 className="text-3xl text-primary font-header font-bold md:text-6xl lg:text-8xl">
+          Nice to meet you!
+          <br />
+          I'm
+          <br />
+          <span className="text-secondary">Somtochukwu</span>.
+        </h1>
+        <em className="opacity-80 text-secondary">_somtodev</em>
+      </div>
+      <p className="text-lg font-light my-4 opacity-75 ">
+        <span className="font-normal">
+          A software engineering student at Aptech.
+        </span>
+        <br />I am passionate about writing digital solutions to problems (the
+        right way). I am an aspiring hardcore backend developer. I'm giving my
+        all to make that a reality :)
+        <br />
+        <br />
+        In my free time, I read books and articles to make me do better as a
+        person and as a developer.
+      </p>
+      <a
+        href="mailto:leroynnalue65@gmail.com"
+        className="px-3 py-2 text-md bg-border bg-opacity-75 rounded-md transition-all hover:bg-opacity-100"
+      >
+        Mail Me
+      </a>
+    </section>
+  );
 }
