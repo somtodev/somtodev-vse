e71c89b515d92e55a2f32a96490ef1a6c846ddc4
somtodev <leroynnalue65@gmail.com>
2023-10-10 18:50:44 +0100

    Update: Changed to random accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
-    theme: {
-        screens: {
-            sm: "640px",
-            md: "768px",
-            lg: "1024px",
-            xl: "1280px",
-            "2xl": "1536px"
-        },
-        colors: {
-           "secondary": "#66CC99",
-            "primary": "#FFFFFF",
-            "border": "#222222",
-            "text": "#DDDDDD",
-            "bg": "#111111",
-        },
-        fontFamily: {
-            header: ["Space Grotesk", "sans-serif"],
-            body: ["Space Grotesk", "sans-serif"],
-        },
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
-    plugins: [],
+    colors: {
+      secondary: "#22F231",
+      primary: "#FFFFFF",
+      border: "#222222",
+      text: "#DDDDDD",
+      bg: "#111111",
+    },
+    fontFamily: {
+      header: ["Space Grotesk", "sans-serif"],
+      body: ["Space Grotesk", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
 };
